0f7f89d48c33a7c9768dd5f22a7e77222070ab27
Philipp Zehnder <zehnder@fzi.de>
2017-12-19 04:56:52 +0100

    Changed domain property that examples work with line chart

--- src/main/java/org/streampipes/examples/sources/vocabulary/WaterTankVocabulary.java
+++ src/main/java/org/streampipes/examples/sources/vocabulary/WaterTankVocabulary.java
@@ -1,19 +1,20 @@
 package org.streampipes.examples.sources.vocabulary;
 
+import org.streampipes.vocabulary.SO;
+
 public class WaterTankVocabulary {
 
   public static final String NS = "https://streampipes.org/vocabulary/examples/watertank/v1/";
-  public static final String NS_PREFIX = "wt";
 
   public static final String HAS_SENSOR_ID = NS + "hasSensorId";
   public static final String IS_OVERFLOW = NS + "isOverflow";
   public static final String IS_UNDERFLOW = NS + "isUnderflow";
-  public static final String HAS_WATER_LEVEL = NS + "hasWaterLevel";
-  public static final String HAS_MASS_FLOW = NS + "hasMassFlow";
-  public static final String HAS_TEMPERATURE = NS + "hasTemperature";
-  public static final String HAS_PRESSURE = NS + "hasPressure";
+  public static final String HAS_WATER_LEVEL = SO.Number;
+  public static final String HAS_MASS_FLOW = SO.Number;
+  public static final String HAS_TEMPERATURE = SO.Number;
+  public static final String HAS_PRESSURE = SO.Number;
 
-  public static final String HAS_VOLUME_FLOW= NS + "hasVolumeFlow";
-  public static final String HAS_SENSOR_FAULT_FLAGS = NS + "hasSensorFaultFlags";
-  public static final String HAS_DENSITY = NS + "hasDensity";
+  public static final String HAS_VOLUME_FLOW= SO.Number;
+  public static final String HAS_SENSOR_FAULT_FLAGS = SO.Number;
+  public static final String HAS_DENSITY = SO.Number;
 }
